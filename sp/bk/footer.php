41d9f0fdc481771347f1a1a1d60159bd7e18da15
uehara-n <37477901+uehara-n@users.noreply.github.com>
2019-03-02 20:07:50 +0900

    Add files via upload

---
+++ sp/bk/footer.php
@@ -0,0 +1,57 @@
+<!--	==================フッター-->
+<footer id="foot"> <a href="#head" class="go_top"><img src="<?php echo get_template_directory_uri(); ?>/images/foot/go_top.svg" alt="トップへ戻る" width="32" height="32"></a>
+
+ 
+
+
+  <div class="com_info inner">
+    <p class="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/foot/logo.png" alt="東海工芸" width="314" height="130"></p>
+    <p class="add">〒468-0014<br>
+      名古屋市天白区中平3-804</p>
+</div>
+<br clear="all">
+  <div class="img_area inner">
+    <p class="ginoushi"><img src="<?php echo get_template_directory_uri(); ?>/images/foot/ginoushi.png" alt="塗装一級技能士" width="324" height="249"></p>
+    <p class="license"><img src="<?php echo get_template_directory_uri(); ?>/images/foot/license.png" alt="許可証" width="324" height="249"></p>
+  </div>
+  <p class="foot_bnr"><img src="<?php echo get_template_directory_uri(); ?>/images/foot/bengo.png" alt="顧問弁護士　敦賀昭代" width="610" height="250"/></p>
+<a href="/maintenance_contact" class="maintenance_btn">お客様専用窓口</a>
+    <p class="map"><img src="<?php echo get_template_directory_uri(); ?>/images/foot/map.png" alt="マップ" width="610" height="543"></p>
+  <div class="area">
+    <p class="tit">【施工エリア】</p>
+    <p class="detail">会社の所在地を中心に愛知県内はもちろん、近隣岐阜県、三重県も場合に応じてご対応致します。<br>
+      愛知県<br>
+      名古屋市（天白区、北区、昭和区、千種区、中区、中川区、西区、東区、瑞穂区、緑区、南区、港区、名東区、守山区）、愛西市、あま市、安城市、一宮市、稲沢市、大府市、岡崎市、尾張旭市、春日井市、刈谷市、北名古屋市、清須市、江南市、小牧市、瀬戸市、高浜市、知多市、知立市、津島市、東海市、常滑市、豊明市、豊田市、長久手市、西尾市、日進市、半田市、碧南市、みよし市、弥富市、東郷町、大治町、蟹江町、阿久比町、美浜町、扶桑町<br>
+      岐阜県<br>
+      多治見市、土岐市、可児市<br>
+      三重県<br>
+      四日市市、鈴鹿市、桑名市、東員町、川越町、菰野町<br>
+      ※車で１時間以内で行ける地域を対象にしています。<br>
+      上記以外のエリアは、ご相談窓口よりお問合せください。</p>
+  </div>
+</footer>
+  <p class="pc-switcher">
+	  <a href="?pc-switcher=1" class="switch_btn more_btn2">PCサイトを表示する</a>
+  </p>
+
+<p class="copyright">Copyright &copy;2017東海工芸. All Rights Reserved.</p>
+<script src="<?php echo get_template_directory_uri(); ?>/js/menu.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/lightbox.js"></script>
+<?php if ( is_home() || is_front_page() ) : ?>
+<script src="<?php echo get_template_directory_uri(); ?>/js/slick.min.js"></script> 
+<script>
+// JavaScript Document
+	$(function() {
+		$('.slide_inner').slick({
+			dots: true,
+			arrows: false,
+			infinite: true,
+			autoplay: true,
+			speed: 300,
+		});
+});
+</script>
+<?php endif; ?>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ sp/bk/index.php
@@ -0,0 +1,288 @@
+<?php get_header(); ?>
+<div class="main_v">
+  <div class="slide_inner">
+    <p class="slide"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/mainv_slide1.jpg" alt="" width="640" height="224"></p>
+    <p class="slide"><a href="<?php bloginfo('url'); ?>/shindan"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/mainv_slide2.jpg" alt="建物診断" width="640" height="224"></a></p>
+    <p class="slide"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/mainv_slide3.jpg" alt="" width="640" height="224"></p>
+  </div>
+</div>
+<main class="mainContents">
+
+<!--<div id="oshirase">
+<strong>東海工芸はゴールデンウィーク期間も通常通り営業致します！</strong><br /><br />
+【ゴールデンウィーク期間中の営業スケジュール】<br />
+2018年5月3日（木）～2018年5月6日（日）通常通り営業しております。<br />
+ゴールデンウィーク中のお問い合わせも大歓迎です！お困りのことやお見積もり相談などお気軽にお問い合わせください。
+</div>-->
+
+  <p class="common_cam_bnr"><a href="/shindan"><img src="<?php echo get_template_directory_uri(); ?>/images/common/cam_bnr.png" alt="おかげさまで20周年記念キャンペーン"></a></p>
+<?php gr_contact_banner(); ?>
+<p class="nav_tit"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/menu_tit.png" alt="menu" width="640" height="62"></p>
+  <nav class="ddmenu vertical close">
+    <ul class="container">
+      <li><a href="#">会社概要<span class="en">COMPANY</span></a>
+          <ul>
+            <li><a href="<?php bloginfo('url'); ?>/company">会社概要</a></li>
+            <li><a href="<?php bloginfo('url'); ?>/staff">スタッフ&職人紹介</a></li>
+            <li><a href="<?php bloginfo('url'); ?>/voice">お客様の声</a></li>
+          </ul>
+      </li>
+      <li><a href="#">東海工芸の特徴<span class="en">ABOUT US</span></a>
+          <ul>
+            <li><a href="<?php bloginfo('url'); ?>/riyu">東海工芸の選ばれる8つの理由</a></li>
+            <li><a href="<?php bloginfo('url'); ?>/yakusoku">東海工芸の6つのお約束</a></li>
+          </ul>
+      </li>
+      <li><a href="#">施工事例<span class="en">WORKS</span></a>
+        <ul>
+	        <li><a href="<?php bloginfo('url'); ?>/seko">すべての施工事例</a></li>
+	        <li><a href="<?php bloginfo('url'); ?>/seko_cat/gaiheki">戸建て 外壁塗装</a></li>
+	        <li><a href="<?php bloginfo('url'); ?>/seko_cat/apart">アパート、マンション 外壁塗装</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/seko_cat/daiwahouse">大和ハウス 施工事例</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/seko_cat/misawahome">ミサワホーム 施工事例</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/seko_cat/panahome">パナホーム 施工事例</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/seko_cat/toyotahome">トヨタホーム 施工事例</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/seko_cat/sekisuihouse">積水ハイム・ハウス 施工事例</a></li>
+			</ul>
+      </li>
+      <li><a href="#">塗料プラン<span class="en">PLAN</span></a>
+        <ul>
+			<li><a href="<?php bloginfo('url'); ?>/silicone">シリコンプラン</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/radical">ラジカルプラン</a></li>
+			<li><a href="<?php bloginfo('url'); ?>t/husso">フッ素プラン</a></li>
+			<li><a href="<?php bloginfo('url'); ?>t/muki">無機プラン</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/dannetsu">断熱プラン</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/hikari">光触媒プラン</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/tateil">プレミアムプラン</a></li>
+      <li><a href="<?php bloginfo('url'); ?>/astec">アステックプラン</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/ceiling2">シーリングプラン</a></li>
+		</ul>
+		</li>
+      <li><a href="<?php bloginfo('url'); ?>/voice">お客様の声<span class="en">VOICE</span></a></li>
+    </ul>
+  </nav>
+	<p class="top_bnr"><a href="<?php bloginfo('url'); ?>/riyu"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/bnr_riyu.png" alt="東海工芸が選ばれる8つの理由" width="640" height="260"></a></p>
+  <p class="top_bnr"><a href="<?php bloginfo('url'); ?>/yakusoku"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/bnr_yakusoku.png" alt="東海工芸は必ず守ります！" width="640" height="231"></a></p>
+
+<p class="top_bnr"><a href="/shiharai"><img src="/wp-content/themes/gaiheki/page_image/top/bnr_aeon.jpg" width="355" height="200" alt="分割ローンで支払い可能" /></a></p>
+
+<p class="top_bnr"><a href="/line"><img src="/wp-content/themes/gaiheki/page_image/top/bnr_line.jpg" width="355" height="200" alt="LINEでアドバイス" /></a></p>
+
+<p class="top_bnr"><a href="/apart"><img src="/wp-content/themes/gaiheki/page_image/top/bnr_apart.jpg" width="355" height="200" alt="アパート・マンション塗装" /></a></p>
+
+<p class="top_bnr"><a href="/kushitsu"><img src="/wp-content/themes/gaiheki/page_image/top/bnr_kushitsu.jpg" width="355" height="200" alt="家賃保証＋外壁塗装で空室リスクなしの賃貸運営を！" /></a></p>
+
+
+
+
+
+
+  <div class="t_from">
+    <img src="<?php echo get_template_directory_uri(); ?>/page_image/top/from_presi.png" alt="私は過去に、リフォーム会社に勤務しておりました。その中で、会社のあり方、工事のあり方、営業マンのあり方、人としてのあり方など自分の理想を考えるようになりました。その結果、自分の理想を追い求める会社を創るべく、独立し、東海工芸を設立しました。おかげさまで今年で設立20年。創業以来、こだわりを持って仕事をさせていただきました。東海工芸は完全自社職人にこだわります！一切下請け業者には依頼をしません！自社職人の教育・管理を徹底して、みなさまのお住いの塗装工事の品質・サービズ向上に日々励んでいます！お客様満足度１００%をこれからも目指していきます！" width="640" height="530">
+  </div>
+ <!-- =================施工事例 -->
+  <section class="t_seko">
+    <h2 class="t_tit"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/seko_tit.png" alt="施工事例" width="640" height="62"></h2>
+    <div class="inner">
+<?php $args = array(
+	'post_type' => 'seko', /* 投稿タイプ */
+	'paged' => $paged,
+	'posts_per_page' => 8 /* 件数表示 */
+	);query_posts( $args );
+if (have_posts()) :	$seko_i = 1;
+while (have_posts()) : the_post();
+/* ループ開始 */ ?>
+      <div class="box">
+        <a href="<?php the_permalink(); ?>"><span class="pic"><?php
+								printf(
+									'%s',
+									gr_get_image(
+										'seko_after_image',
+										array( 'width' => 240, 'alt' => esc_attr( post_custom( 'seko_comment' ) ) )
+									)
+								);
+								?></span> <span class="tit"><?php the_title(); ?></span><span class="name"><?php echo post_custom( 'seko_city' ); ?>　<?php echo post_custom( 'seko_name' ); ?></span></a>
+      </div>
+<?php
+$seko_i++; //最後にループ回数を一つ進める
+endwhile;
+endif;
+wp_reset_query(); ?>
+    </div>
+    <p class="more"><a href="<?php bloginfo('url'); ?>/seko"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/seko_more.png" alt="施工事例をもっと見る" width="580" height="60"></a></p>
+  </section>
+ <!-- =================/施工事例 -->
+<?php gr_contact_banner(); ?>
+<!-- =================お客様の声 -->
+<section class="t_voice">
+    <h2 class="t_tit"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/voice_tit.png" alt="お客様の声" width="640" height="62"></h2>
+    <div class="inner">
+<?php $args = array(
+	'post_type' => 'voice', /* 投稿タイプ */
+	'paged' => $paged,
+	'posts_per_page' => 2 /* 件数表示 */
+	);query_posts( $args );
+if (have_posts()) :
+while (have_posts()) : the_post();
+/* ループ開始 */ ?>
+
+      <div class="box">
+        <a href="<?php the_permalink(); ?>"><span class="pic">
+						<?php if(post_custom( 'voice_image')):
+						$imagefield = get_imagefield( 'voice_image' );
+						$thumb = wp_get_attachment_image_src( $imagefield[ 'id' ], 'large' );
+						printf(
+							'%2$s',
+							$thumb[ 0 ],
+							gr_get_image(
+								'voice_image',
+								array(
+									'width' => 240,
+								)
+							),
+							get_the_title()
+						);?>
+						<?
+						else:
+						?>
+						<?php
+						$imagefield = get_imagefield( 'voice_image01' );
+						$thumb = wp_get_attachment_image_src( $imagefield[ 'id' ], 'large' );
+						printf(
+							'%2$s',
+							$thumb[ 0 ],
+							gr_get_image(
+								'voice_image01',
+								array(
+									'width' => 240,
+								)
+							),
+							get_the_title()
+						);
+						?><?php endif; ?>
+</span> <span class="tit"><?php the_title(); ?></span>
+<?php if(post_custom( 'voice_user_message' )){?>
+	<span class="text"><?php
+     $str = (post_custom('voice_user_message', array()));
+     $count = 40; // 制限したい文字数
+     $more = '…';
+     echo wp_html_excerpt( $str, $count, $more );
+?></span><?}?></a>
+      </div>
+<?php
+endwhile;endif;
+wp_reset_query(); ?>
+    </div>
+    <p class="more"><a href="<?php bloginfo('url'); ?>/voice"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/voice_more.png" alt="お客様の声をもっと見る" width="580" height="60"></a></p>
+  </section>
+<!-- =================/お客様の声 -->
+  <section class="t_blog">
+    <h2 class="t_tit"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/blog_tit.png" alt="ブログ" width="128" height="33"></h2>
+    <article class="in_block t_genba">
+      <div class="detail">
+        <h3 class="blog_tit"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/blog_genba_tit.png" alt="現場リポート" width="320" height="62"></h3>
+<?php
+$the_query = new WP_Query();
+$args = array(
+	'post_type' => 'blog',
+	'posts_per_page' => 1, /* 件数表示 */
+	'tax_query' => array(
+		array(
+			'taxonomy' => 'blog_cat',
+			'field' => 'slug',
+			'terms' => 'genba',
+		)
+	)
+);
+$the_query->query($args);
+if($the_query->have_posts()): while($the_query->have_posts()) : $the_query->the_post();
+?>
+
+        <h4 class="blog_tit"><? the_title(); ?></h4>
+        <p class="text"><a href="<?php the_permalink(); ?>"><?php
+if(mb_strlen($post->post_content,'UTF-8')>50){
+	$content= str_replace('\n', '', mb_substr(strip_tags($post-> post_content), 0, 50,'UTF-8'));
+	echo $content.'……';
+}else{
+	echo str_replace('\n', '', strip_tags($post->post_content));
+}
+?></a></p>
+
+      </div>
+      <p class="pic"><a href="<?php the_permalink(); ?>"><img src="<?php echo catch_that_image(); ?>" alt="" width="234"></a></p>
+<?php endwhile;endif; ?>
+<?php wp_reset_query(); ?>
+
+    </article>
+    <article class="in_block t_president">
+      <div class="detail">
+        <h3 class="blog_tit"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/blog_presi_tit.png" alt="社長ブログ" width="320" height="62"></h3>
+<?php
+$the_query = new WP_Query();
+$args = array(
+	'post_type' => 'blog',
+	'posts_per_page' => 1, /* 件数表示 */
+	'tax_query' => array(
+		array(
+			'taxonomy' => 'blog_cat',
+			'field' => 'slug',
+			'terms' => 'presi',
+		)
+	)
+);
+$the_query->query($args);
+if($the_query->have_posts()): while($the_query->have_posts()) : $the_query->the_post();
+?>
+
+        <h4 class="blog_tit"><? the_title(); ?></h4>
+        <p class="text"><a href="<?php the_permalink(); ?>"><?php
+if(mb_strlen($post->post_content,'UTF-8')>50){
+	$content= str_replace('\n', '', mb_substr(strip_tags($post-> post_content), 0, 50,'UTF-8'));
+	echo $content.'……';
+}else{
+	echo str_replace('\n', '', strip_tags($post->post_content));
+}
+?></a></p>
+      </div>
+      <p class="pic"><a href="<?php the_permalink(); ?>"><img src="<?php echo catch_that_image(); ?>" alt="" width="234"></a></p>
+<?php endwhile;endif; ?>
+<?php wp_reset_query(); ?>
+    </article>
+    <article class="in_block t_staff">
+      <div class="detail">
+        <h3 class="blog_tit"><img src="<?php echo get_template_directory_uri(); ?>/page_image/top/blog_staff_tit.png" alt="スタッフブログ" width="320" height="62"></h3>
+<?php
+$the_query = new WP_Query();
+$args = array(
+	'post_type' => 'blog',
+	'posts_per_page' => 1, /* 件数表示 */
+	'tax_query' => array(
+		array(
+			'taxonomy' => 'blog_cat',
+			'field' => 'slug',
+			'terms' => array( 'genba','presi' ),
+			'operator'  => 'NOT IN'
+		)
+	)
+);
+$the_query->query($args);
+if($the_query->have_posts()): while($the_query->have_posts()) : $the_query->the_post();
+?>
+        <h4 class="blog_tit"><? the_title(); ?></h4>
+        <p class="text"><a href="<?php the_permalink(); ?>"><?php
+if(mb_strlen($post->post_content,'UTF-8')>50){
+	$content= str_replace('\n', '', mb_substr(strip_tags($post-> post_content), 0, 50,'UTF-8'));
+	echo $content.'……';
+}else{
+	echo str_replace('\n', '', strip_tags($post->post_content));
+}
+?></a></p>
+      </div>
+      <p class="pic"><a href="<?php the_permalink(); ?>"><img src="<?php echo catch_that_image(); ?>" alt="" width="234"></a></p>
+<?php endwhile;endif; ?>
+<?php wp_reset_query(); ?>
+
+    </article>
+  </section>
+</main>
+<?php get_footer(); ?>
